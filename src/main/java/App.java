27845c263e32d054f9d59bf6b68c93d946da9357
studentkat <122557985+studentkat@users.noreply.github.com>
2023-03-21 11:02:03 +0500

    pp-2-1-2

---
+++ src/main/java/App.java
@@ -0,0 +1,31 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getMessage());
+
+
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(beanCat.equals(beanCat2));
+
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name;
+
+    public String getMessage() {
+        return name;
+    }
+
+    public void setMessage(String name) {
+        this.name = name;
+    }
+}
